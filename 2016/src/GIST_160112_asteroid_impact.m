52fbd90da29e6869501bca897d425b0fd940dfd3
Griffin Chure <gchure@caltech.edu>
2018-01-03 17:30:40 -0800

    Added 2018

---
+++ 2016/src/GIST_160112_asteroid_impact.m
@@ -0,0 +1,19 @@
+% Define radius
+r = 1:1000000; % meters
+
+% Initialize vector to save energies
+energy = zeros(1, length(r));
+
+% Define constants
+v = 10^3; % m / s
+rho = 3000; % kg / m^3
+
+% loop through r to calculate energy
+for i=1:length(r)
+    energy(i) = 2 * rho * r(i)^3 * v^2;
+end
+
+% Plot on a log-log scale
+loglog(r, energy)
+xlabel('radius [m]')
+ylabel('energy [J]')
---
+++ push_papers.sh
@@ -0,0 +1,1 @@
+scp -r _publications/*.pdf  rpdata@ssh.caltech.edu:public_html/course_papers/protected/
